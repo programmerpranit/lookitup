6af1c402e7384c381edae54ab62f1bf4cd67231a
Pranit Patil <dev.pranitpatil@gmail.com>
2022-04-09 15:22:47 +0530

    creating req adapter

--- app/src/main/java/com/psp/lookitup/ui/adapters/RequestAdapter.kt
+++ app/src/main/java/com/psp/lookitup/ui/adapters/RequestAdapter.kt
@@ -1,14 +1,49 @@
 package com.psp.lookitup.ui.adapters
 
-import com.firebase.ui.firestore.FirestoreRecyclerAdapter
-import com.firebase.ui.firestore.FirestoreRecyclerOptions
 
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.psp.lookitup.R
 import com.psp.lookitup.data.Request
 
-class RequestAdapter() : FirestoreRe
+class QuestionsAdapter : ListAdapter<Request, QuestionsAdapter.QuestionViewHolder>(DataComparator()) {
 
+//    inner class QuestionViewHolder(
+//        private val binding: ItemQuestionBinding
+//        ) : RecyclerView.ViewHolder(binding.root)
 
+    inner class QuestionViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val title:TextView = itemView.findViewById(R.id.)
+        // val name:TextView = itemView.findViewById(R.id.tvUserNameInMRV)
+        //val time:TextView = itemView.findViewById(R.id.tvTimeInMRV)
+    }
 
-interface IPostAdapter{
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): QuestionViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_question, parent, false)
+        return QuestionViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: QuestionViewHolder, position: Int) {
+        val currentItem = getItem(position)
+        holder.title.text = currentItem.question_text
+        //holder.name.text = currentItem.user.name
+        // holder.time.text = currentItem.timestamp
+    }
+
+    class DataComparator : DiffUtil.ItemCallback<Question>() {
+        override fun areItemsTheSame(oldItem: Question, newItem: Question): Boolean {
+            return oldItem.qid == newItem.qid
+        }
+
+        override fun areContentsTheSame(oldItem: Question, newItem: Question): Boolean {
+            return oldItem == newItem
+        }
+
+    }
 
 }
--- app/src/main/java/com/psp/lookitup/ui/fragments/MainFragment.kt
+++ app/src/main/java/com/psp/lookitup/ui/fragments/MainFragment.kt
@@ -33,4 +33,9 @@ class MainFragment : Fragment(R.layout.fragment_main) {
 
     }
 
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
