f972621256e92ebcf6e395b8592bab7be3ff3124
Prathamesh Karambelkar <pkarambelkar.2002@gmail.com>
2022-04-09 11:38:51 +0530

    mandatory

--- app/src/main/java/com/psp/lookitup/data/User.kt
+++ app/src/main/java/com/psp/lookitup/data/User.kt
@@ -4,13 +4,13 @@ import java.text.DateFormat
 
 data class User(
     val id: Int,
-    val name: String,
-    val emailId: String,
-    val DOB: DateFormat,
-    val gender: String,
-    val Status: String,
-    val occupation: String,
-    val location: String
+    val name: String? = null,
+    val emailId: String? = null,
+    val DOB: DateFormat? = null,
+    val gender: String?= null,
+    val Status: String? = null,
+    val occupation: String? = null,
+    val location: String? = null
 
 )
 
