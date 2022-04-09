b821588713b3d7e2f0b03d5ccec51f546e6ffc61
Pranit Patil <dev.pranitpatil@gmail.com>
2022-04-09 11:38:18 +0530

    added binding

--- app/src/main/java/com/psp/lookitup/ui/viewmodels/MainViewmodel.kt
+++ app/src/main/java/com/psp/lookitup/ui/viewmodels/MainViewmodel.kt
@@ -2,7 +2,8 @@ package com.psp.lookitup.ui.viewmodels
 
 import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
 
 @HiltViewModel
-class MainViewmodel: ViewModel() {
+class MainViewmodel @Inject constructor(): ViewModel() {
 }
