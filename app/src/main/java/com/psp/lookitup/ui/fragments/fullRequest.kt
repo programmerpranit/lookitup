0c50c7dfe8ace87bb253a7b5e0cbf8d942525a2c
Prathamesh Karambelkar <pkarambelkar.2002@gmail.com>
2022-04-10 00:14:12 +0530

    Sync COmmit

--- app/src/main/java/com/psp/lookitup/ui/fragments/fullRequest.kt
+++ app/src/main/java/com/psp/lookitup/ui/fragments/fullRequest.kt
@@ -1,14 +1,22 @@
 package com.psp.lookitup.ui.fragments
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 import com.psp.lookitup.R
 
 
 class fullRequest : Fragment() {
 
+    val TAG = "Full Request Fragment"
+    val dbref = Firebase.firestore
+    val intent = Intent(Intent.ACTION_SEND)
 
 }
